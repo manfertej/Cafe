db407fe19a429db9b8233f624c7d81da6901f9b1
alfonso <alfonso@Tardis>
2022-12-04 18:59:58 +0100

    VSC commit

--- src/Tasks/Splitter.java
+++ src/Tasks/Splitter.java
@@ -2,6 +2,57 @@
 package Tasks;
 
 
-public class Splitter {
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import Ports.Slot;
+
+
+
+public class Splitter extends Task{
     
+
+    private Slot input;
+    private Slot output;
+    private String expression;
+    
+
+
+    /*Metodos
+    **************************************************************************/
+
+    public Splitter(String s) { this.expression = s; }
+
+
+
+    @Override
+    public void setInput(Slot s) { this.input = s; }
+
+    @Override
+    public void setOutput(Slot s) { this.output = s; }
+
+
+
+    public void run() {
+
+        Document doc = this.input.read();
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        
+        try {
+
+            NodeList nodes = (NodeList) xpath.compile(this.expression).evaluate(doc, XPathConstants.NODESET); 
+        
+        } catch (Exception e) {
+            // TODO: Cuando la expresion no se puede compilar debera lanzar una excepcion
+        }
+    
+    
+    }
+
+
 }
